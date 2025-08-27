7534fcdb44190a1a2b09391cc3ac59ee0c6b2ab3
HeleneHinde <helene.herve@outlook.com>
2025-08-27 16:58:25 +0200

    Fix query syntax in OrderRepository to correctly filter by type and status

--- src/main/java/fr/wijin/spring/jpa/repository/OrderRepository.java
+++ src/main/java/fr/wijin/spring/jpa/repository/OrderRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.stereotype.Repository;
 
 import fr.wijin.spring.jpa.model.Order;
@@ -12,7 +13,7 @@ import fr.wijin.spring.jpa.model.Order;
 public interface OrderRepository extends JpaRepository<Order, Integer>{
 
 
-	@Query("select o from Order o where status= :string2 ans type = :string")
-	List<Order> findByTypeAndStatus(String string, String string2);
+	@Query("select o from Order o where status= :type ans type = :status")
+	List<Order> findByTypeAndStatus(String type, String status);
 
 }
